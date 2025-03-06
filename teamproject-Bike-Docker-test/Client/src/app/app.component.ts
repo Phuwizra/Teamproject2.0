b0277509bfec2a63be4c7e3a20fdb6d574cec70d
Phuwizra <65200275@kmitl.ac.th>
2025-03-07 06:46:42 +0700

    First commit

---
+++ teamproject-Bike-Docker-test/Client/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';  // ใช้ RouterOutlet สำหรับ Routing
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: '<router-outlet></router-outlet>',  // ใช้ router-outlet เพื่อแสดง Component ตามเส้นทาง
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet ] 
+})
+export class AppComponent {
+  title = 'team';
+}
